1da54e0b9739cdaa34cd43e37b7cdfb996219898
Damian <damian.pugliese@mercadolibre.com>
2021-06-13 21:08:35 -0300

    first commit

---
+++ client/webpack.config.js
@@ -0,0 +1,61 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.join(__dirname, '/build/static'),
+        filename: 'js/[name].bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                loader: 'file-loader',
+                options: {
+                    name: './media/images/[name].[ext]',
+                },
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader',
+                ],
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            filename: '../index.html',
+            inject: true,
+            // favicon: "./src/public/favicon.ico"
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'css/styles.css'
+        })
+    ],
+    devServer: {
+        // proxy: {
+        //     "/api": {
+        //         target: "http://localhost:5000"
+        //     }
+        // },
+        contentBase: path.resolve(__dirname, '/build/static'),
+        watchContentBase: true,
+        port: 3000,
+        hot: true,
+        historyApiFallback: true,
+        open: true
+    }
+}
